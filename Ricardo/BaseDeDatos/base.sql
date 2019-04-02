6639c9460658eafc84b68bd8305d68a594d0b299
Ricardo <shirifcam@gmail.com>
2019-04-02 14:57:20 -0600

    nuevo

--- Ricardo/BaseDeDatos/base.sql
+++ Ricardo/BaseDeDatos/base.sql
@@ -1,42 +1,41 @@
 DROP SCHEMA IF EXISTS Base CASCADE;
 CREATE SCHEMA Base;
 CREATE TABLE Base.usuario(
-	idUsuario INT NOT NULL AUTO_INCREMENT,
+	idUsuario serial primary key,
 	nombreUsuario text NOT NULL,
 	contraseña text NOT NULL,
 	correo text NOT NULL,
-	tipo int NOT NULL,
-	CONSTRAINT id_Usuario_pkey PRIMARY KEY(idUsuario)
+	tipo text NOT NULL,
+	CONSTRAINT correo unique(correo)
 );
 
 CREATE TABLE Base.tema(
-	idTema INT NOT NULL AUTO_INCREMENT,
-	idUsuario INT NOT NULL,
+	idTema Serial primary key,
+	idUsuario integer NOT NULL,
 	nombreTema text NOT NULL,
-	CONSTRAINT id_tema_pkey PRIMARY KEY(idTema),
 	CONSTRAINT fk_idUsuario FOREIGN KEY (idUsuario) REFERENCES Base.usuario(idUsuario)
 );
 
 CREATE TABLE Base.marcador(
-	idMarcador INT NOT NULL AUTO_INCREMENT,
-	idTema INT NOT NULL,
+	idMarcador Serial primary key,
+	idTema integer NOT NULL,
 	latitud FLOAT NOT NULL,
 	longitud FLOAT NOT NULL,
 	datos_utiles text NOT NULL,
 	descripcion text NOT NULL,
 	color int NOT NULL,
-	CONSTRAINT id_Marcador_pkey PRIMARY KEY(idMarcador,idTema),
+	CONSTRAINT id_Marcador_pkey unique(idMarcador,idTema),
 	CONSTRAINT fk_idTema FOREIGN KEY (idTema) REFERENCES Base.tema(idTema)
 );
 
 CREATE TABLE Base.comentario(
-	idComentario INT NOT NULL AUTO_INCREMENT ,
-	idUsuario INT not NULL,
-	idMarcador INT not NULL,
-	idTema INT not NULL,
+	idComentario serial primary key,
+	idUsuario integer not NULL,
+	idMarcador integer not NULL,
+	idTema integer not NULL,
 	comentario text NOT NULL,
 	fecha date,
-	CONSTRAINT id_comentario_pkey PRIMARY KEY(idComentario,idMarcador,idTema),
+	CONSTRAINT id_comentario_pkey unique(idComentario,idMarcador,idTema),
 	CONSTRAINT fk_idComeentario FOREIGN KEY(idMarcador,idTema) REFERENCES Base.marcador(idMarcador,idTema)
 );
 	
