4782d94a7907fb321aec24ddea7f56a92ea2d635
Shirifcam <shirifcam@gmail.com>
2019-04-05 17:55:05 -0600

    Base nueva

--- BaseDeDatos/base.sql
+++ BaseDeDatos/base.sql
@@ -1,42 +1,43 @@
 DROP SCHEMA IF EXISTS Base CASCADE;
 CREATE SCHEMA Base;
-
 CREATE TABLE Base.usuario(
-	idUsuario int NOT NULL,
+	idUsuario serial primary key,
 	nombreUsuario text NOT NULL,
 	contraseña text NOT NULL,
 	correo text NOT NULL,
 	tipo text NOT NULL,
-	CONSTRAINT id_Usuario_pkey PRIMARY KEY(idUsuario)
+	CONSTRAINT correo unique(correo),
+	CONSTRAINT nombreU unique(nombreUsuario)
 );
 
 CREATE TABLE Base.tema(
-	idTema int NOT NULL,
-	idUsuario int NOT NULL,
+	idTema Serial primary key,
+	idUsuario integer NOT NULL,
 	nombreTema text NOT NULL,
-	CONSTRAINT id_tema_pkey PRIMARY KEY(idTema),
-	CONSTRAINT fk_idUsuario FOREIGN KEY (idUsuario) REFERENCES Base.usuario(idUsuario)
+	CONSTRAINT fk_idUsuario FOREIGN KEY (idUsuario) REFERENCES Base.usuario(idUsuario) ON DELETE CASCADE,
+	CONSTRAINT nombre unique(nombreTema)
 );
 
 CREATE TABLE Base.marcador(
-	idMarcador int NOT NULL,
-	idTema int NOT NULL,
-	ubicación text NOT NULL,
+	idMarcador Serial primary key,
+	idTema integer NOT NULL,
+	latitud FLOAT NOT NULL,
+	longitud FLOAT NOT NULL,
 	datos_utiles text NOT NULL,
 	descripcion text NOT NULL,
 	color int NOT NULL,
-	CONSTRAINT id_Marcador_pkey PRIMARY KEY(idMarcador,idTema),
-	CONSTRAINT fk_idTema FOREIGN KEY (idTema) REFERENCES Base.tema(idTema)
+	CONSTRAINT id_Marcador_pkey unique(idMarcador,idTema),
+	CONSTRAINT fk_idTema FOREIGN KEY (idTema) REFERENCES Base.tema(idTema) ON DELETE CASCADE
 );
 
 CREATE TABLE Base.comentario(
-	idComentario int NOT NULL,
-	idUsuario int not NULL,
-	idMarcador int not NULL,
-	idTema int not NULL,
+	idComentario serial primary key,
+	idUsuario integer not NULL,
+	idMarcador integer not NULL,
+	idTema integer not NULL,
 	comentario text NOT NULL,
 	fecha date,
-	CONSTRAINT id_comentario_pkey PRIMARY KEY(idComentario,idMarcador,idTema),
-	CONSTRAINT fk_idComeentario FOREIGN KEY(idMarcador,idTema) REFERENCES Base.marcador(idMarcador,idTema)
+	CONSTRAINT id_comentario_pkey unique(idComentario,idMarcador,idTema),
+	CONSTRAINT fk_idComeentario FOREIGN KEY(idMarcador,idTema) REFERENCES Base.marcador(idMarcador,idTema) ON DELETE CASCADE
 );
 	
